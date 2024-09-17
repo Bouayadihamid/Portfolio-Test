b576d185db331eb0c5fc2ac031c8240f47d9bf6b
Hamid <Bouayadihamid@gmail.com>
2024-09-17 04:09:33 +0100

    Some work get done

--- Backend/main.py
+++ Backend/main.py
@@ -1,27 +1,49 @@
-from flask import Flask,request
+from flask import Flask,request,jsonify
 from flask_restx import Api,Resource,fields
 from config import DevConfig
-from models import Recipe
+from models import Recipe,User
 from exts import db
-
+from flask_migrate import Migrate
+from werkzeug.security import generate_password_hash,check_password_hash
+from flask_jwt_extended import JWTManager, create_access_token, create_fresh_token
 
 app = Flask(__name__)
 app.config.from_object(DevConfig)
 
 db.init_app(app)
 
+migrate=Migrate(app,db)
+JWTManager(app)
+
 api=Api(app,doc='/docs')
 
 #model (serialization)
-recipe_model=api.model()
-"Recipe",
-{
+recipe_model=api.model(
+    "Recipe",
+    {
         "id":fields.Integer(),
         "title":fields.String(),
         "description":fields.String()
     }
+)
+
+signup_model=api.model(
+    'Signup',
+    {
+        "username":fields.String(),
+        "email":fields.String(),
+        "password":fields.String()
+    }
+)
 
 
+login_model=api.model(
+    'Login',
+    {
+       'username':fields.String(),
+       'password':fields.String() 
+    }
+)
 @api.route('/hello')
 class HelloResource(Resource):
     def get(self):
@@ -29,8 +51,51 @@ class HelloResource(Resource):
 
 
 
+@api.route('/signup')
+class SignUp(Resource):
+    @api.expect(signup_model)
+    def post(self):
+        data=request.get_json()
 
-@api.router('/recipes')
+        username=data.get('username')
+
+        db_user=User.query.filter_by(username=username).first()
+
+        if db_user is not None:
+            return jsonify({"message":f"User with username {username} already exists"})
+
+        new_user=User(
+            username=data.get('username'),
+            email=data.get('email'),
+            password=generate_password_hash(data.get('password'))
+        )
+
+        new_user.save()
+
+        return jsonify({"message":"User created succefully"})
+    
+
+@api.route('/login')
+class Login(Resource):
+    @api.expect(login_model)
+    def post(self):
+        data=request.get_json()
+
+        username=data.get('username')
+        password=data.get('password')
+
+        db_user=User.query.filer_by(username=username).first()
+
+        if db_user and check_password_hash(db_user.password, password):
+            create_access_token=create_access_token(identity=db_user.username)
+            refresh_token=create_access_token(identity=db_user.username)
+            return jsonify(
+                {"access token":access_token,"refresh_token":refresh_token}
+            )
+
+
+
+@api.route('/recipes')
 class RecipesResource(Resource):
 
     @api.marshal_list_with(recipe_model)
@@ -42,6 +107,7 @@ class RecipesResource(Resource):
 
         return recipes
     @api.marshal_with(recipe_model)
+    @api.expect(recipe_model)
     def post(self):
         """Create a new recipe"""
 
@@ -56,7 +122,7 @@ class RecipesResource(Resource):
         
         return new_recipe,201
 
-@api.rout('/recipe/<int:id>')
+@api.route('/recipe/<int:id>')
 class RecipeResource(Resource):
 
     @api.marshal_with(recipe_model)
